5e5be1b2e06185cd536de8bb2900fd14998d80dd
Tomasz Dobrowolski <jarv@monochrome.pl>
2023-05-31 14:22:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ const CONFIG = {
 	greetingNight: 'Go to Sleep!',
 
 	// Layout
-	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+	bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
 
 	// Weather
 	weatherKey: '1aa35c110e8eef3033e6be4f435d9c8d', // Write here your API Key
