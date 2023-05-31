7713cf22da75f0049adf932b58aa9a6b7abfde0a
Tomasz Dobrowolski <jarv@monochrome.pl>
2023-05-31 15:48:36 +0200

    update config.js

--- config.js
+++ config.js
@@ -65,9 +65,9 @@ const CONFIG = {
 		},
 		{
 			id: '3',
-			name: 'Todoist',
-			icon: 'trello',
-			link: 'https://todoist.com',
+			name: 'Feedly',
+			icon: 'rss',
+			link: 'https://feedly.com/',
 		},
 		{
 			id: '4',
